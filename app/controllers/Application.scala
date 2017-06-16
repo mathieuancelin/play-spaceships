19dc1f559ab622871cf73202716f0cb84be3d785
dsagnier <sagnier.dimitri@gmail.com>
2017-06-16 16:00:16 +0200

    Page for add new player and control page for each player

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,22 +3,34 @@ package controllers
 import play.api._
 import play.api.mvc._
 import models._
-import scala.collection.mutable.ListBuffer
+import play.api.libs.json._
 
 class Application extends Controller {
 
   val game = new Game()
-  var players = new ListBuffer[Player]
+  var players = Seq.empty[Player]
 
   def board = Action { implicit request =>
     game.start()
-    Ok(views.html.board(game))
+    Ok(views.html.board(game,players))
   }
 
-  def controller = Action { implicit request =>
-    players+=new Player("test",1,1)
-    players+=new Player("test2",1,1)
-    Ok(views.html.control(players.toList))
+  def mobileStart = Action { implicit request =>
+    Ok(views.html.mobilestart())
+  }
+
+  def addNewPlayer(username: String) = Action {
+    players = players :+ Player(username,1,1)
+    Ok(JsArray(players.map(p => Json.obj("name" -> p.name))))
+  }
+
+  def getPlayer = Action {
+    val json = Json.toJson(players.map(p => Json.obj("name" -> p.name)))
+    Ok(json)
+  }
+
+  def controller(username: String) = Action { implicit request =>
+    Ok(views.html.control(username))
   }
 
 }
